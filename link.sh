b6aafe74314f0fb210cf394eb83464f3a0029e70
0xbitx <74537225+0xbitx@users.noreply.github.com>
2023-12-14 03:03:58 -0500

    Delete link.sh

--- link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mask="https://facebook.com"
-link=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' ".link.log")
-link1=${link#https://}
-echo -e "\n [-] URL 1 : $link"
-echo -e "\n [-] URL 2 : $mask@$link1"
-echo "----------------------------------------------------------------------------------------------"
